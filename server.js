5a2d2b6d2c4343f6492b17065b43d91b25c81a92
owenThurm <owen.p.thurm@gmail.com>
2020-11-05 22:02:06 -0500

    Working advice api call and advice routing to GET /

--- server.js
+++ server.js
@@ -4,14 +4,36 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
 app.get('/', (req, res) => {
   console.log('hello world');
+  axios({
+    method: 'get',
+    url: 'https://api.adviceslip.com/advice',
+    responseType: 'JSON'
+  }).then(response => {
+    console.log(response);
+    const advice = response.data.slip.advice;
+    const adviceNum = response.data.slip.id;
+    console.log(advice);
+    console.log('Words of wisdom #' + adviceNum);
+    try{
+      res.write(advice);
+      res.write('\n \n \t -Words of Wisdom #' + adviceNum);
+      res.end();
+    } catch(error) {
+      console.log(error);
+    }
+  }).catch(err => {
+    console.log(err);
+  })
 
-  const advice = axios.get();
-
-
-  res.send('hello world');
-  res.end();
 });
 
 app.listen(port, err => {
